bdc520423bd2556ee47e96ef34fe2345c1691a56
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2025-09-04 19:54:46 -0300

    desafio

--- Media-aluno/media.c
+++ Media-aluno/media.c
@@ -5,5 +5,13 @@ int main() {
     float nota2;
     float media;
 
-    
+    printf("Insira a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Insira a segunda nota: ");
+    scanf("%f", &nota2);
+
+    media = (nota1 + nota2) / 2;
+
+    printf("A média das suas notas é de: %f\n", media);
 }
